b1752c391e9079bc2ade553e903acb3db6913b56
Abdulkareem Ridwan Opeyemi <55938645+abuphawzww@users.noreply.github.com>
2022-07-26 18:05:06 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,42 +1,26 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *argstostr - concatenates all arguements to the program
- * @ac: arguement count
- * @av: pointer to arguements
- * Return: pointer to new space in memory or null
+ * *create_array - creates an array of chars, and initializes it with a char
+ * @size: size of the array
+ * @c: char to be initialized with
+ * Return: pointer to array or null
  **/
-char *argstostr(int ac, char **av)
+char *create_array(unsigned int size, char c)
 {
-	char *strDup;
-	int i, j, k, size;
+	char *str;
+	unsigned int i;
 
-	if (ac == 0 || av == NULL)
+	if (size == 0)
 		return (NULL);
-	size = 0;
-/* count the number of chars in each string */
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			size++;
-		size++;
-	}
-	size++;
-/* allocate memory for total number of chars and
- * new line for each word
- */
-	strDup = malloc(sizeof(char) * size);
-	if (strDup == NULL)
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
 		return (NULL);
-	k = 0;
-	for (i = 0; i < ac; i++)
+	i = 0;
+	while (i < size)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			strDup[k++] = av[i][j];
-		}
-		strDup[k++] = '\n';
+		str[i] = c;
+		i++;
 	}
-	strDup[k] = '\0';
-	return (strDup);
+	return (str);
 }
