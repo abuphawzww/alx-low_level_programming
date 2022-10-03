7155169c132be92d090dc23a793fe7098a229111
Abdulkareem Ridwan Opeyemi <55938645+abuphawzww@users.noreply.github.com>
2022-10-03 14:08:50 -0700

    Update 0-main.cc

--- 0x18-dynamic_libraries/0-main.cc
+++ 0x18-dynamic_libraries/0-main.cc
@@ -1,1 +1,13 @@
-iiiiiiii
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
